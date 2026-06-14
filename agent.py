3d33b56d7bad6af976ea10588c6eeda4d13745b5
Bharrguv <bharrguvarmn@gmail.com>
2026-06-14 21:11:36 +0530

    My latest changes

--- agent.py
+++ agent.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 llm=ChatGroq(
-    model = "llama-3.3-70b-versatile",
+    model = "meta-llama/llama-4-scout-17b-16e-instruct",
     temperature=0
 )
 
